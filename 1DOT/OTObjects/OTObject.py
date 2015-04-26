c842f1e445dc72ee664c67dd2cf4679cb8038d19
aFarchi <alban.farchi@free.fr>
2015-04-26 18:27:07 +0200

    initial commit for 1D problem

---
+++ 1DOT/OTObjects/OTObject.py
@@ -0,0 +1,13 @@
+################
+# Class OTObject
+################
+#
+# Default class for all objects 
+# Stores the grid discretization
+#
+
+class OTObject:
+    
+    def __init__(self, N, P):
+        self.N = N
+        self.P = P
