c43164eeb2430557d2316a00f97aabf47bdec30d
aFarchi <alban.farchi@free.fr>
2015-04-26 19:13:37 +0200

    added code for class CenteredGrid

--- 1DOT/OTObjects/grid/grid.py
+++ 1DOT/OTObjects/grid/grid.py
@@ -156,8 +156,8 @@ class StaggeredField( Field ):
         f[:,:]           = 0.5*self.f[:,0:self.P+1]
         f[:,0:self.P+1] += 0.5*self.f[:,1:self.P+2]
 
-#        return CenteredField( self.N, self.P,
-#                             m, f )
+        return CenteredField( self.N, self.P,
+                              m, f )
 
     def divergence(self):
         div = ( self.N*( self.m[1:self.N+2,:] - self.m[0:self.N+1,:] ) +
@@ -167,3 +167,39 @@ class StaggeredField( Field ):
     # Boundary function
     # DivBound function
     # DivTempBound function
+
+class CenteredField( Field ):
+    '''
+    Class to handle a field defined on a centered grid
+    '''
+
+    def __init__( self ,
+                  N , P ,
+                  m=None , f=None ):
+        if m is None:
+            m = np.zeros(shape=(N+1,P+1))
+        if f is None:
+            f = np.zeros(shape=(N+1,P+1))
+
+        Field.__init__( self ,
+                        N , P ,
+                        m , f )
+
+    def __repr__(self):
+        return 'Object representing a field (m,f) on a centered grid'
+
+    def functionalJ(self):
+        return ( ( self.m * self.m ) *
+                 ( self.f > 0 ) / self.f ).sum()
+
+    def Tinterpolation(self):
+        m = np.zeros(shape=(N+2,P+1))
+        m[0:self.N+1,:]  = 0.5*self.m[:,:]
+        m[1:self.N+2,:] += 0.5*self.m[:,:]
+
+        f = np.zeros(shape=(N+1,P+2))
+        f[:,0:self.P+1]  = 0.5*self.f[:,:]
+        f[:,1:self.P+2] += 0.5*self.f[:,:]
+
+        return StaggeredField( N, P,
+                               m, f )
