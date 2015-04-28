ab8536f5e9d751c12a7deee7d27c1ab8b17f20c7
aFarchi <alban.farchi@free.fr>
2015-04-28 16:47:14 +0200

    added proximal operator for boundary conditions constrain with a reservoir

---
+++ 1DOT/OTObjects/proximals/bound/proxCrb.py
@@ -0,0 +1,60 @@
+###############
+# Class ProxCrb
+###############
+#
+# Projector on the boundary condition constrain with reservoir
+#
+
+import time as tm
+from .. import projector as proj
+from ...grid import grid
+
+class ProxCrb( proj.Projector ):
+    '''
+    Projector on the boundary condition constrain with reservoir
+    '''
+    
+    def __init__( self ,
+                  N , P ,
+                  kernel=None ):
+        if kernel is None:
+            kernel = grid.Boundaries(N,P)
+
+        self.massDefault = kernel.massDefault()
+        proj.Projector.__init__( self ,
+                                 N , P ,
+                                 kernel )
+
+    def __repr__(self):
+        return ( 'Projector on the boundary contion constrain space with reservoir.' )
+
+    def __call__(self, field, overwrite=True):
+        if overwrite:
+            field.m[0,:]        = self.kernel.spatialBoundaries.bx0[:]
+            field.m[self.N+1,:] = self.kernel.spatialBoundaries.bx1[:]
+
+            field.f[1:self.N,0]        = self.kernel.temporalBoundaries.bt0[1:self.N]
+            field.f[1:self.N,self.P+1] = self.kernel.temporalBoundaries.bt1[1:self.N]
+
+            field.f[0,0]      = 0.
+            field.f[self.N,0] = 0.
+
+            deltaMassCurrent = field.boundaries().massDefault() - self.massDefault
+
+            deltaMassCurrent /= 2. * self.P
+
+            field.f[0,self.P+1]      += deltaMassCurrent
+            field.f[self.N,self.P+1] += deltaMassCurrent
+
+            return field
+        else:
+            return self(field.copy(), True)
+
+    def timing(self,nTiming,overwrite=True):
+        t = 0.
+        for i in xrange(nTiming):
+            field = grid.StaggeredField.random(self.N, self.P)
+            time_start = tm.time()
+            field = self(field,overwrite)
+            t += tm.time() - time_start
+        return t
