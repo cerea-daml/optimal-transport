ca85d668725b269497f2b07536c46e8531bd2422
aFarchi <alban.farchi@free.fr>
2015-04-29 11:43:47 +0200

    added run function to class AdrAlgorithm

--- OT1D/OTObjects/adr/adrAlgorithm.py
+++ OT1D/OTObjects/adr/adrAlgorithm.py
@@ -11,6 +11,12 @@
 #   * outputDir
 #
 #   * iterCount
+#   * iterTarget
+#
+#   * printConfig()
+#
+#   * nModPrint
+#   * nModWrite
 #
 
 import pickle as pck
@@ -42,14 +48,11 @@ class AdrAlgorithm( oto.OTObject ):
     def __repr__(self):
             return ( 'ADR algorithm' )
 
-    def saveState(self, outputDir=None):
-        if outputDir is None:
-            outputDir = self.config.outputDir
-
-        fileConfig   = outputDir + 'config.bin'
-        fileBoundary = outputDir + 'boundary.init'
-        fileState    = outputDir + 'finalState.bin'
-        fileRunCount = outputDir + 'runCount.bin'
+    def saveState(self):
+        fileConfig   = self.config.outputDir + 'config.bin'
+        fileBoundary = self.config.outputDir + 'boundary.init'
+        fileState    = self.config.outputDir + 'finalState.bin'
+        fileRunCount = self.config.outputDir + 'runCount.bin'
 
         f = open(fileConfig, 'ab')
         p = pck.Pickler(f)
@@ -73,6 +76,9 @@ class AdrAlgorithm( oto.OTObject ):
         except:
             runCount = np.ones(1)
             runCount.tofile(fileRunCount)
+
+        self.config.iterCount = 0
+        self.config.iterTarget = 0
 
     def setState(self, newState):
         self.stateN = newState
@@ -102,6 +108,43 @@ class AdrAlgorithm( oto.OTObject ):
         self.config.iterCount = 0
 
     def run(self):
-        # TO WRITE ...
-        pass
+        fileCurrentState = self.config.outputDir + 'states.bin'
     
+        f = open(fileCurrentState, 'ab')
+        p = pck.Pickler(f)
+
+        print('__________________________________________________')
+        print('Starting Adr algorithm...')
+        print('__________________________________________________')
+        self.config.printConfig()
+        print('__________________________________________________')
+        timeStart = tm.time()
+        
+        while self.config.iterCount < self.config.iterTarget:
+            self.stepFunction(self.stateN,self.stateNP1)
+            self.stepFunction(self.stateNP1,self.stateNP)
+
+            if np.mod(self.config.iterCount, self.config.nModPrint) == 0:
+                print('___________________________________')
+                print('iteration   : '+str(self.config.iterCount)+'/'+str(self.config.iterTarget))
+                print('elpsed time : '+str(tm.time()-timeStart))
+                print('J = ',str(self.stateN.functionalJ()))
+
+            if np.mod(self.config.iterCount, self.config.nModWrite) == 0:
+                p.dump(self.stateN)
+            self.config.iterCount += 2
+
+        timeAlgo = tm.time() - timeStart
+        f.close()
+        finalJ = self.stateN.functionalJ()
+
+        print('__________________________________________________')
+        print('Adr algorithm finished')
+        print('Number of iterations run : '+str(self.config.iterTarget))
+        print('Final J = '+str(finalJ))
+        print('Time taken : '+str(timeAlgo))
+        print('Mean time per iteration : '+str(timeAlgo/iterTarget))
+        print('__________________________________________________')
+
+        self.saveState()
+        return finalJ
