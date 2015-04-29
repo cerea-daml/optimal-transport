39540fdc2abce0f86355bc09ad6f6e44b45d2afc
aFarchi <alban.farchi@free.fr>
2015-04-29 10:05:07 +0200

    added functions to give initial fields for a given config

---
+++ OT1D/OTObjects/init/initialFields.py
@@ -0,0 +1,51 @@
+##################
+# initialFields.py
+##################
+#
+# defines initial fields for a given configuration
+#
+# config must define :
+#   * N
+#   * P
+#   * boundaries
+#   * dynamics
+#
+
+import numpy as np
+from ..grid import grid
+
+def initialStaggeredField(config):
+    m = np.zeros(shape=(config.N+2,config.P+1))
+    f = np.zeros(shape=(config.N+1,config.P+2))
+
+    if config.dynamics == 0:
+
+        for i in xrange(config.P+2):
+            t = float(i)/(config.P+1.)
+            f[:,i] = ( config.boundaries.temporalBoundaries.bt0[:] * ( 1. - t ) + 
+                       config.boundaries.temporalBoundaries.bt1[:] * t )
+
+        massIncoming = ( ( float(config.N) / float(config.P) ) * 
+                         np.cumsum( config.boundaries.spatialBoundaries.bx0 - 
+                                    config.boundaries.spatialBoundaries.bx1 ) )
+
+        for i in xrange(config.P+1):
+            f[:,i+1] += massIncoming[i] / ( config.N + 1. )
+
+    else:
+    
+        for i in xrange(config.P+2):
+            t = float(i)/(config.P+1.)
+            f[:,i] = ( config.boundaries.temporalBoundaries.bt0[:] * ( 1. - t ) +
+                       config.boundaries.temporalBoundaries.bt1[:] * t )
+            
+    return grid.StaggeredField( config.N , config.P ,
+                                m , f )
+
+def initialCenteredField(config):
+    return initialStaggeredField(config).interpolation()
+
+def initialStaggeredCenteredField(config):
+    staggeredField = initialStaggeredField(config)
+    return grid.StaggeredCenteredField( config.N , config.P ,
+                                        staggeredField , staggeredField.interpolation() )
