c013e7c144572beaa5b281966312d7b936aa58e8
aFarchi <alban.farchi@free.fr>
2015-05-05 23:24:07 +0200

    fixed minor issues

--- OT1D/OTObjects2D/plotting/plotAnalyseMultiSim.py
+++ OT1D/OTObjects2D/plotting/plotAnalyseMultiSim.py
@@ -2,28 +2,40 @@
 # plotAnalyse.py
 ################
 #
-# plots the result of an analyse
+# plots the result of multiple analyses
 #
 
 import numpy as np
 from matplotlib import pyplot as plt
 import cPickle as pck
 
-def plotAnalyse(outputDir, figDir, prefixFig='analyse', figSubFig=None):
+def plotAnalyseMultiSim(outputDirList, figDir, prefixFig='analyse', labelsList=None, figSubFig=None):
 
-    options = ['b-','g-','r-','m-','y-','c-','k-']
+    options = ['b-', 'g-', 'r-', 'm-', 'y-', 'c-', 'k-',
+               'b--','g--','r--','m--','y--','c--','k--',
+               'b:', 'g:', 'r:', 'm:', 'y:', 'c:', 'k:' ]
+
     nModOptions = len(options)
 
-    fileAnalyse = outputDir + 'analyse.bin'
-    f = open(fileAnalyse, 'rb')
-    p = pck.Unpickler(f)
-    iterationNumbers = p.load()
-    iterationTimes = p.load()
-    names = p.load()
-    values = p.load()
-    f.close()
+    numbers = []
+    times   = []
+    values  = []
+    for outputDir in outputDirList:
+        fileAnalyse = outputDir + 'analyse.bin'
+        f = open(fileAnalyse, 'rb')
+        p = pck.Unpickler(f)
+        numbers.append(p.load())
+        times.append(p.load())
+        names = p.load()
+        values.append(p.load())
+        f.close()
 
     N  = len(names)
+
+    if labelsList is None:
+        labelsList = []
+        for i in xrange(len(outputDirList)):
+            labelsList.append('sim '+str(i)+', ')
 
     if figSubFig is None:
         figSubFig = []
@@ -36,10 +48,10 @@ def plotAnalyse(outputDir, figDir, prefixFig='analyse', figSubFig=None):
         nbrSubFig = len(fig)
         Nc = int(np.floor(np.sqrt(nbrSubFig)))
         Nl = Nc
-        while Nc*Nl < nbrSubFig:
+        while Nl*Nc < nbrSubFig:
             Nl += 1
 
-        figure = plt.figure()#figsize=(10*Nl,10*Nc))            
+        figure = plt.figure()            
         plt.clf()
 
         j = 1
@@ -55,18 +67,19 @@ def plotAnalyse(outputDir, figDir, prefixFig='analyse', figSubFig=None):
             ax.set_xlabel(xlabel)
             ax.set_ylabel(ylabel)
 
-            if xaxis == 'iterations':
-                X = iterationNumbers
-            elif xaxis == 'time':
-                X = iterationTimes
-
             nOptions = 0
-            for column in columns:
-                column = min(N-1, column)
-                Y = values[:,column]
-                ax.plot(X,Y,options[nOptions],label=names[column])
-                nOptions = np.mod(nOptions+1,nModOptions)
-                
+            for value,it,t,label in zip(values,numbers,times,labelsList):
+                if xaxis == 'iterations':
+                    X = it
+                elif xaxis == 'time':
+                    X = t
+
+                for column in columns:
+                    column = min(N-1, column)
+                    Y = value[:,column]
+                    ax.plot(X,Y,options[nOptions],label=label+names[column])
+                    nOptions = np.mod(nOptions+1,nModOptions)
+
             from mpl_toolkits.axes_grid1 import make_axes_locatable
             divider = make_axes_locatable(ax)
             lax = divider.append_axes('right', '10%',frameon=False)
@@ -87,23 +100,22 @@ def plotAnalyse(outputDir, figDir, prefixFig='analyse', figSubFig=None):
         plt.savefig(figName)
         i += 1
 
-def plotAnalyseDefaultSubplots(outputDir, figDir, prefixFig='analyse', itOrTime='iterations'):
+def plotAnalyseMultiSimDefaultSubplots(outputDirList, figDir, prefixFig='analyse', itOrTime='iterations', labelsList=None):
 
     figSubFig = [ [ ( [0] , itOrTime , 'log' , 'log' , itOrTime , '$div$' , 'Divergence constrain' , True , 'DivConstrain' ) ] ,
                   [ ( [1] , itOrTime , 'log' , 'log' , itOrTime , '$abs(min(.))$' , 'Positivity constrain' , True , 'PosConstrain' ) ] ,
                   [ ( [2] , itOrTime , 'log' , 'log' , itOrTime , '$J$' , 'Cost function' , True , 'J' ) ] ,
                   [ ( [2,3,4,5,6] , itOrTime , 'log' , 'log' , itOrTime , '$J$' , 'Cost function' , True , 'moreJ' ) ] ,
                   [ ( [7] , itOrTime , 'log' , 'log' , itOrTime , '' , 'Convergence' , True , 'Convergence' ) ] ]
+    
+    plotAnalyseMultiSim(outputDirList, figDir, prefixFig, labelsList, figSubFig)
 
-    plotAnalyse(outputDir, figDir, prefixFig, figSubFig)
-
-def plotAnalyseDefaultSubplots2(outputDir, figDir, prefixFig='analyse', itOrTime='iterations'):
+def plotAnalyseMultiSimDefaultSubplots2(outputDirList, figDir, prefixFig='analyse', itOrTime='iterations', labelsList=None):
 
     figSubFig = [ [ ( [0] , itOrTime , 'log' , 'log' , itOrTime , '$div$' , 'Divergence constrain' , True , 'DivConstrain' ) ,
-                    ( [1] , itOrTime , 'log' , 'log' , itOrTime , '$abs(min(.))$' , 'Positivity constrain' , True , 'Constrains' ) ] ,
-                  [ ( [2] , itOrTime , 'log' , 'log' , itOrTime , '$J$' , 'Cost function' , True , 'J' )  ,
-                    ( [2,3,4,5,6] , itOrTime , 'log' , 'log' , itOrTime , '$J$' , 'Cost function' , True , 'J' ) ] ,
+                    ( [1] , itOrTime , 'log' , 'log' , itOrTime , '$abs(min(.))$' , 'Positivity constrain' , True , 'PosConstrain' ) ] ,
+                  [ ( [2] , itOrTime , 'log' , 'log' , itOrTime , '$J$' , 'Cost function' , True , 'J' ) ,
+                    ( [2,3,4,5,6] , itOrTime , 'log' , 'log' , itOrTime , '$J$' , 'Cost function' , True , 'moreJ' ) ] ,
                   [ ( [7] , itOrTime , 'log' , 'log' , itOrTime , '' , 'Convergence' , True , 'Convergence' ) ] ]
-
-    plotAnalyse(outputDir, figDir, prefixFig, figSubFig)
-
+    
+    plotAnalyseMultiSim(outputDirList, figDir, prefixFig, labelsList, figSubFig)
