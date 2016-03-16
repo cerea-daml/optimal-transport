8b462a899c5783848097ac11e675125769249c86
aFarchi <alban.farchi@free.fr>
2016-03-16 21:23:56 +0100

    added cr

--- OT/OTObjects2D/algorithms/adr/prox1Adr.py
+++ OT/OTObjects2D/algorithms/adr/prox1Adr.py
@@ -1,3 +1,16 @@
+#==================================================
+#__________________________________________________
+
+# Copyrigth 2016 A. Farchi and M. Bocquet
+# CEREA, joint laboratory Ecole des Ponts ParisTech and EDF R&D
+
+# Code for the paper: Using the Wasserstein distance to compare fields of pollutants:
+# Application to the radionuclide atmospheric dispersion of the Fukushima-Daiichi accident
+# by A. Farchi, M. Bocquet, Y. Roustan, A. Mathieu and A. Querel
+
+#__________________________________________________
+#==================================================
+
 ################
 # Class Prox1Adr
 ################
